c9de66df0d1c6c264d18a545ac6527809edfc363
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-19 12:32:49 -0500

    Generic Functions

--- README.md
+++ README.md
@@ -4,3 +4,8 @@
 
 Built in ways to define your data which allows for further manipulation on data. Type validation that allows you to maintain code will work as expected
 `const names: Array<string> = ["Chris","Jen"]`
+
+### Generic Functions
+
+`function merge<T, U>(objA: T, objB: U){return Object.assign(objA, objB)`
+`objA` & `objB` can be of different type datas and the return will be the intersection of data inputs. These are dyamically set when function is called
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
-const names: Array<string> = ["Jen", "Chris"]; //same as string[]
+/*const names: Array<string> = ["Jen", "Chris"]; //same as string[]
 // names[0].split(" ");
 const promise: Promise<string> = new Promise((resolve, reject) => {
   setTimeout(() => {
     resolve("completed!");
   }, 2000);
 });
+*/
+// Generic Function
+function merge<T, U>(objA: T, objB: U) {
+  return Object.assign(objA, objB);
+}
+console.log(merge({ name: "max" }, { age: 25 }));
+const mergedObject = merge({ name: "max" }, { age: 25 });
+// mergedObject.name; // Cannot target
+console.log(mergedObject.age); //Can target with Generic T, U
+const mergedObject2 = merge<{ name: string; hobby: string[] }, { age: number }>(
+  { name: "Cindy", hobby: ["dancing"] },
+  { age: 27 }
+);
+console.log(mergedObject2);
