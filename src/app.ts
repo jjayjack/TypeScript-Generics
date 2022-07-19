5b2be19faa341aeb6cbf3923703e814316cc4898
Jocelyn Jayjack <jocelynjayjack@gmail.com>
2022-07-19 15:18:57 -0500

    Generic Classes

--- src/app.ts
+++ src/app.ts
@@ -53,3 +53,36 @@ function extractAndConvert<T extends Object, U extends keyof T>(
   return "Value: " + obj[key];
 }
 extractAndConvert({ name: "Max" }, "name");
+
+// Generic Classes
+class DataStorage<T extends string | number | boolean> {
+  private data: T[] = [];
+  addItem(item: T) {
+    this.data.push(item);
+  }
+  removeItem(item: T) {
+    if (this.data.indexOf(item) === -1) {
+      return;
+    }
+    this.data.splice(this.data.indexOf(item), 1);
+  }
+  getItems() {
+    return [...this.data];
+  }
+}
+
+const textStorage = new DataStorage<string>();
+textStorage.addItem("Max");
+textStorage.addItem("Manuel");
+textStorage.removeItem("Max");
+console.log(textStorage.getItems());
+
+const numberStorage = new DataStorage<number>();
+/*
+const objStorage = new DataStorage<object>();
+objStorage.addItem({ name: "Max" });
+objStorage.addItem({ name: "Travis" });
+//
+objStorage.removeItem({ name: "Max" });
+console.log(objStorage.getItems());
+*/
